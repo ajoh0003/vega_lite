25c8d2d18a4f7025ac904faf987f3b7aaa86ef52
ajoh0003 <64778003+ajoh0003@users.noreply.github.com>
2021-09-28 21:15:09 +1000

    Delete 1_map directory

--- 1_map/js/vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "data/earthquake_map.json";
-vegaEmbed("#map", vg_1, {actions: false}).then(function(result) {
- // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
